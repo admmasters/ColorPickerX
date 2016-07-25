a87b3ba5b31c3977177bf130e3581a445a57bbfe
Matt Revell <mattrevell82@me.com>
2016-07-25 19:16:34 +0100

    Initial Commit

---
+++ ColorPickerX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ColorPickerX
+//
+//  Created by Matthew Revell on 25/07/2016.
+//  Copyright © 2016 Matthew Revell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
